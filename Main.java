ad1cdf714062ce638d2ad0e7d8185d6034993677
Deekshithab <127728585+Deekshithab@users.noreply.github.com>
2023-04-04 19:04:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.gl.javafsd.dsa.paymoney;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			
+				PayMoneyTransactionManager manager = new PayMoneyTransactionManager();
+				
+				manager.process();
+			}
+		}
+
+
